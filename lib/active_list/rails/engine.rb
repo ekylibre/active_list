8ad1a2432483d4e0ab98554e667f5e7609c26ff6
Brice Texier <burisu@oneiros.fr>
2014-08-06 11:14:49 +0200

    Moves locales at their good place

--- lib/active_list/rails/engine.rb
+++ lib/active_list/rails/engine.rb
@@ -5,7 +5,7 @@ module ActiveList
       initializer "active_list.integrate_methods" do |app|
         ::ActionController::Base.send(:include, ActiveList::Rails::Integration::ActionController)
         ::ActionView::Base.send(:include, ActiveList::Rails::Integration::ViewsHelper)
-        files = Dir[File.join(File.dirname(__FILE__), "..", "..", "..", "locales", "*.yml")]
+        files = Dir[File.join(File.dirname(__FILE__), "..", "..", "..", "config", "locales", "*.yml")]
         ::I18n.load_path.concat(files)
       end
     end
