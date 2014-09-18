b1c2af4e1b61701d354557134cdbb83bda78323e
Brice Texier <burisu@oneiros.fr>
2014-09-18 14:04:51 +0200

    Reorganizes locales to ensure detection

--- lib/active_list/rails/engine.rb
+++ lib/active_list/rails/engine.rb
@@ -5,9 +5,10 @@ module ActiveList
       initializer "active_list.integrate_methods" do |app|
         ::ActionController::Base.send(:include, ActiveList::Rails::Integration::ActionController)
         ::ActionView::Base.send(:include, ActiveList::Rails::Integration::ViewsHelper)
-        # files = Dir[Pathname.new(__FILE__).dirname.join("..", "..", "..", "config", "locales", "*.yml")]
-        # puts ::I18n.load_path.map(&:red).to_sentence
-        # ::I18n.load_path.concat(files)
+        # Adds locales
+        for file in Dir.glob(Pathname.new(__FILE__).dirname.join("..", "locales", "*.yml"))
+          ::I18n.load_path << file unless ::I18n.load_path.include?(file)
+        end
       end
     end
   end
--- lib/active_list/version.rb
+++ lib/active_list/version.rb
@@ -1,5 +1,5 @@
 module ActiveList
 
-  VERSION = "6.2.2"
+  VERSION = "6.2.3"
 
 end
