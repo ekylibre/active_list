0e359d898a59651afe4fe51140755a6bf4858900
Rémi Piotaix <remi@piotaix.fr>
2019-03-28 09:37:18 +0100

    Monkeypatch for tests on 2.6

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -23,6 +23,22 @@ ActionMailer::Base.delivery_method = :test
 ActionMailer::Base.perform_deliveries = true
 ActionMailer::Base.default_url_options[:host] = 'test.com'
 
+# Patch from https://github.com/rails/rails/issues/34790#issuecomment-450502805
+if RUBY_VERSION >= '2.6.0'
+  if Rails.version < '5'
+    class ActionController::TestResponse < ActionDispatch::TestResponse
+      def recycle!
+        # hack to avoid MonitorMixin double-initialize error:
+        @mon_mutex_owner_object_id = nil
+        @mon_mutex = nil
+        initialize
+      end
+    end
+  else
+    puts "Monkeypatch for ActionController::TestResponse no longer needed"
+  end
+end
+
 module ActionView
   class Base
     module Nomen
